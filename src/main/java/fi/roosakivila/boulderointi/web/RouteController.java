7477b752a384c033515a0b45de73b8416261c47d
Roosa Kivilä <roosakivila@Roosa-MacBook-Air.local>
2025-10-24 17:24:01 +0300

    RouteControlleriin ensimmäinen GET pyyntö Routelist, routelist.html template, bootstrapin lisäys

--- src/main/java/fi/roosakivila/boulderointi/web/RouteController.java
+++ src/main/java/fi/roosakivila/boulderointi/web/RouteController.java
@@ -1,8 +1,28 @@
 package fi.roosakivila.boulderointi.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import fi.roosakivila.boulderointi.domain.GymRepository;
+import fi.roosakivila.boulderointi.domain.RouteRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class RouteController {
+
+    private RouteRepository routeRepository;
+    private GymRepository gymRepository;
+
+    public RouteController(RouteRepository routeRepository, GymRepository gymRepository) {
+        this.routeRepository = routeRepository;
+        this.gymRepository = gymRepository;
+    }
+
+    @GetMapping("/routelist")
+    public String routeList(Model model) {
+        model.addAttribute("routes", routeRepository.findAll());
+        return "routelist"; // routelist.html
+    }
 
 }
