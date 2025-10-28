8bbd112b2339e1afaaa22dc218dc254a220a37f4
Roosa Kivilä <roosakivila@Roosa-MacBook-Air.local>
2025-10-28 13:40:44 +0200

    Gym controller, gymlist, add, save, delete ja edit gym sekä thymleaf templatit. Project controllerin aloitus

--- src/main/java/fi/roosakivila/boulderointi/web/GymController.java
+++ src/main/java/fi/roosakivila/boulderointi/web/GymController.java
@@ -1,8 +1,58 @@
 package fi.roosakivila.boulderointi.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import fi.roosakivila.boulderointi.domain.Gym;
+import fi.roosakivila.boulderointi.domain.GymRepository;
 
 @Controller
 public class GymController {
 
+    private GymRepository gymRepository;
+
+    public GymController(GymRepository gymRepository) {
+        this.gymRepository = gymRepository;
+    }
+
+    // Get gymlist
+    @GetMapping("/gymlist")
+    public String gymList(Model model) {
+        model.addAttribute("gyms", gymRepository.findAll());
+        return "gymlist"; // gymlist.html
+    }
+
+    // Add gym
+    @RequestMapping("/addgym")
+    public String addGym(Model model) {
+        model.addAttribute("gym", new Gym());
+        return "addgym"; // addgym.html
+    }
+
+    // Save gym
+    @PostMapping("/savegym")
+    public String saveGym(Gym gym) {
+        gymRepository.save(gym);
+        return "redirect:gymlist";
+    }
+
+    // Delete gym
+    @GetMapping("/deletegym/{id}")
+    public String deleteGym(@PathVariable("id") Long id, Model model) {
+        gymRepository.deleteById(id);
+        return "redirect:../gymlist";
+    }
+
+    //  Edit gym
+    @GetMapping("/editgym/{id}")
+    public String modifyGym(@PathVariable("id") Long id, Model model) {
+        Gym gym = gymRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid gym Id:" + id));
+        model.addAttribute("gym", gym);
+        return "editgym"; // editgym.html
+    }
 }
--- src/main/java/fi/roosakivila/boulderointi/web/ProjectController.java
+++ src/main/java/fi/roosakivila/boulderointi/web/ProjectController.java
@@ -2,7 +2,22 @@ package fi.roosakivila.boulderointi.web;
 
 import org.springframework.stereotype.Controller;
 
+import fi.roosakivila.boulderointi.domain.GymRepository;
+import fi.roosakivila.boulderointi.domain.ProjectRepository;
+import fi.roosakivila.boulderointi.domain.RouteRepository;
+
 @Controller
 public class ProjectController {
+
+    private ProjectRepository projectRepository;
+    private RouteRepository routeRepository;
+    private GymRepository gymRepository;
+
+    public ProjectController(ProjectRepository projectRepository,
+            RouteRepository routeRepository, GymRepository gymRepository) {
+        this.projectRepository = projectRepository;
+        this.routeRepository = routeRepository;
+        this.gymRepository = gymRepository;
+    }
 
 }
