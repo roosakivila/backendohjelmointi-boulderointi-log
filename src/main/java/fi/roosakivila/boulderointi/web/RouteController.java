3a3ad918e3252e9577ff7ec0d1e1a03075f60b34
Roosa Kivilä <roosakivila@Roosa-MacBook-Air.local>
2025-10-28 13:06:08 +0200

    Controllerit add, save, delete ja edit route ja näihin thymeleaf templatit

--- src/main/java/fi/roosakivila/boulderointi/web/RouteController.java
+++ src/main/java/fi/roosakivila/boulderointi/web/RouteController.java
@@ -4,8 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
 import fi.roosakivila.boulderointi.domain.GymRepository;
+import fi.roosakivila.boulderointi.domain.Route;
 import fi.roosakivila.boulderointi.domain.RouteRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -19,10 +23,42 @@ public class RouteController {
         this.gymRepository = gymRepository;
     }
 
+    // Get routelist
     @GetMapping("/routelist")
     public String routeList(Model model) {
         model.addAttribute("routes", routeRepository.findAll());
         return "routelist"; // routelist.html
     }
 
+    // Add route
+    @RequestMapping("/addroute")
+    public String addRoute(Model model) {
+        model.addAttribute("route", new Route());
+        model.addAttribute("gyms", gymRepository.findAll());
+        return "addroute"; // addroute.html
+    }
+
+    // Save route
+    @PostMapping("/saveroute")
+    public String saveRoute(Route route) {
+        routeRepository.save(route);
+        return "redirect:routelist";
+    }
+
+    // Delete route
+    @GetMapping("/delete/{id}")
+    public String deleteRoute(@PathVariable("id") Long id, Model model) {
+        routeRepository.deleteById(id);
+        return "redirect:../routelist";
+    }
+
+    //  Edit route
+    @GetMapping("/edit/{id}")
+    public String modifyBook(@PathVariable("id") Long id, Model model) {
+        Route route = routeRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid route Id:" + id));
+        model.addAttribute("route", route);
+        model.addAttribute("gyms", gymRepository.findAll());
+        return "editroute"; // editroute.html
+    }
 }
