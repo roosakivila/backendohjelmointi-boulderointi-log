8ab1ed53470603627cd36ebe9e04df8225ad5dbe
Roosa Kivilä <roosakivila@Roosa-MacBook-Air.local>
2025-11-08 15:48:02 +0200

    I18n kielenvaihto suomi-englanti, message.properties keys asetettu, templatet muokattu, kielen vaihtopainike navigaatioon

---
+++ src/main/java/fi/roosakivila/boulderointi/LocaleConfig.java
@@ -0,0 +1,34 @@
+package fi.roosakivila.boulderointi;
+
+import java.util.Locale;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+@Configuration
+public class LocaleConfig implements WebMvcConfigurer {
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver resolver = new SessionLocaleResolver();
+        resolver.setDefaultLocale(new Locale("en")); // Default to English
+        return resolver;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
+        interceptor.setParamName("lang"); // URL parameter: ?lang=fi or ?lang=en
+        return interceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+}
